ab65d1a8db46cad477dd6e3bc39010cde801dfcf
Torge Riggers <trigger@stud.hs-bremen.de>
2016-05-18 16:54:34 +0200

    Test News String

---
+++ src/main/java/Webassistent/NewsService.java
@@ -0,0 +1,18 @@
+package Webassistent;
+
+public class NewsService {
+	private String urlendpoint = "https://gateway-a.watsonplatform.net/calls/data/GetNews";
+	private String apikey = "?apikey=8c616a12601d88cbc6db0688ea4d65bf9ae267f0";
+	
+	public void News(){		
+		String outputmode ="&outputMode=json";
+		String period ="&start=now-1d&end=now";
+		String para ="&q.enriched.url.text=baseball";
+		String re ="&return=enriched.url.text,original.url";
+		
+		JsonConnector conector = new JsonConnector();
+		System.out.println(urlendpoint + apikey + outputmode + period + para + re);
+		conector.getJsonFromURL(urlendpoint + apikey + outputmode + period + para + re);
+		
+	}
+}
