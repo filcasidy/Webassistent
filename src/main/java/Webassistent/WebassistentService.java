80c87d272172553151bfa41ba926c4823d912c7e
Torge Riggers <trigger@stud.hs-bremen.de>
2016-06-12 21:27:08 +0200

    parameter übergabe

--- src/main/java/Webassistent/WebassistentService.java
+++ src/main/java/Webassistent/WebassistentService.java
@@ -13,56 +13,52 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-@Api(
-        name = "webassistent",
-        version = "v1",
-        scopes = {Constants.EMAIL_SCOPE},
-        clientIds = {Constants.WEB_CLIENT_ID, Constants.ANDROID_CLIENT_ID, Constants.IOS_CLIENT_ID},
-        audiences = {Constants.ANDROID_AUDIENCE}
-)
+@Api(name = "webassistent", version = "v1", scopes = { Constants.EMAIL_SCOPE }, clientIds = { Constants.WEB_CLIENT_ID,
+		Constants.ANDROID_CLIENT_ID, Constants.IOS_CLIENT_ID }, audiences = { Constants.ANDROID_AUDIENCE })
 public class WebassistentService {
 
-    private List<IService> services = new ArrayList<>();
-    public static ArrayList<Serverresponse> suggestions = new ArrayList<>();
+	private List<IService> services = new ArrayList<>();
+	public static ArrayList<Serverresponse> suggestions = new ArrayList<>();
 
-    public WebassistentService() {
-        this.services.add(new BundesligaService());
-        this.services.add(new WeatherService());
-        this.services.add(new NewsService());
+	public WebassistentService() {
+		this.services.add(new BundesligaService());
+		this.services.add(new WeatherService());
+		this.services.add(new NewsService());
 
-        for (IService service : services) {
-            for (String command : service.getCommands())
-                suggestions.add(new Serverresponse(command));
-        }
-    }
+		for (IService service : services) {
+			for (String command : service.getCommands())
+				suggestions.add(new Serverresponse(command));
+		}
+	}
 
-    public Serverresponse getServerresponse(@Named("id") String id) throws NotFoundException {
-    	String[] arr = id.split(",");
-    	List<String> para = new LinkedList<String>();
-    	for (int i = 1; i < arr.length; i++){
-			 para.add(arr[i]);
-		 }
-        try {
-            String rVal = "";
-            for (IService service : services) {
-                if (service.hasCommand(arr[0])) {
-                    rVal = service.getServiceResponse(id, para).toString();
-                }
-            }
-            return new Serverresponse(rVal);
+	public Serverresponse getServerresponse(@Named("id") String id) throws NotFoundException {
+		String[] arr = id.split(",");
+		List<String> para = new LinkedList<String>();
+		String command = arr[0];
+		for (int i = 1; i < arr.length; i++) {	
+			para.add(arr[i].replace(" ", ""));
+		}
+		try {
+			String rVal = "";
+			for (IService service : services) {
+				if (service.hasCommand(command)) {
+					rVal = service.getServiceResponse(command, para).toString();
+				}
+			}
+			return new Serverresponse(rVal);
 
-        } catch (IndexOutOfBoundsException e) {
-            throw new NotFoundException("Error in Service");
-        }
-    }
+		} catch (IndexOutOfBoundsException e) {
+			throw new NotFoundException("Error in Service");
+		}
+	}
 
-    /**
-     * Gets all sugestions (commands).
-     *
-     * @return arraylist with all sugestions (commands)
-     */
-    public ArrayList<Serverresponse> getSuggestions() {
-        return suggestions;
-    }
+	/**
+	 * Gets all sugestions (commands).
+	 *
+	 * @return arraylist with all sugestions (commands)
+	 */
+	public ArrayList<Serverresponse> getSuggestions() {
+		return suggestions;
+	}
 
 }
