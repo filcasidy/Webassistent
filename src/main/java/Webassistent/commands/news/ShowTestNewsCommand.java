ecff034309dd5cc3451df679e9f0847ac8c2154f
Torge Riggers <trigger@stud.hs-bremen.de>
2016-06-23 13:03:05 +0200

    Show news

--- src/main/java/Webassistent/commands/news/ShowTestNewsCommand.java
+++ src/main/java/Webassistent/commands/news/ShowTestNewsCommand.java
@@ -17,42 +17,45 @@ public class ShowTestNewsCommand implements ICommand {
 	@Override
 	public Object execute(List<String> parameter) {
 		Document document = null;
-		JSONObject mainobject = JsonUtils.readJsonFromUrlToJsonObject(getNews(parameter.get(parameter.size() - 1)));
-		System.out.println(getNews(parameter.get(parameter.size() - 1)));
+		if (parameter.size() > 0) {
+			JSONObject mainobject = JsonUtils.readJsonFromUrlToJsonObject(getNews(parameter.get(parameter.size() - 1)));
+			System.out.println(getNews(parameter.get(parameter.size() - 1)));
 
-		System.out.println(mainobject);
-		if (mainobject.get("status").equals("OK")) {
-			JSONObject result = mainobject.getJSONObject("result");
-			String title = "Nothing found", text = null, url = null;
+			System.out.println(mainobject);
+			if (mainobject.get("status").equals("OK")) {
+				JSONObject result = mainobject.getJSONObject("result");
+				String title = "Nothing found", text = null, url = null;
 
-			if (result.has("docs")) {
-				org.json.JSONArray docs = result.getJSONArray("docs");
+				if (result.has("docs")) {
+					org.json.JSONArray docs = result.getJSONArray("docs");
 
-				for (int i = 0; i < docs.length(); i++) {
-					JSONObject jsonObj = docs.getJSONObject(i);
-					title = JsonUtils.getJson(jsonObj, "source.enriched.url.title").toString();
-					text = JsonUtils.getJson(jsonObj, "source.enriched.url.text").toString();
-					url = JsonUtils.getJson(jsonObj, "source.original.url").toString();
-					document = HtmlCreatorUtils.createPanel(title, text, url) ;
-					System.out.println(HtmlCreatorUtils.createPanel(title, text, url).toString());
+					for (int i = 0; i < docs.length(); i++) {
+						JSONObject jsonObj = docs.getJSONObject(i);
+						title = JsonUtils.getJson(jsonObj, "source.enriched.url.title").toString();
+						text = JsonUtils.getJson(jsonObj, "source.enriched.url.text").toString();
+						url = JsonUtils.getJson(jsonObj, "source.original.url").toString();
+						document = HtmlCreatorUtils.createPanel(title, text, url);
+						System.out.println(HtmlCreatorUtils.createPanel(title, text, url).toString());
+					}
+
+				} else {
+					document = HtmlCreatorUtils.createPanel("No news found");
 				}
-
+				return document;
 			} else {
-				document = HtmlCreatorUtils.createPanel("No news found");
+				if (mainobject.get("status").equals("ERROR")) {
+					return mainobject.getString("status") + ":" + mainobject.getString("statusInfo");
+				}
 			}
-			return document;
-		} else {
-			if (mainobject.get("status").equals("ERROR")) {
-				return mainobject.getString("status") + ":" + mainobject.getString("statusInfo");
-			}
+			return mainobject;
 		}
-		return mainobject;
+		return "Bitte Such Kriterium angeben";
 
 	}
 
 	public String getNews(String thema) {
 
-		String period = "&start=now-6d&end=now&count=10";
+		String period = "&start=now-6d&end=now&count=2";
 		String para = "&q.enriched.url.title=" + thema;
 		String re = "&return=enriched.url.text,original.url,enriched.url.title";
 
